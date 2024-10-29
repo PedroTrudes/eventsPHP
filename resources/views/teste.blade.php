0784d5a3ad13c766062abf9c168984984fa89b7c
PedroTrudes <pedro.jtrudes@gmail.com>
2024-10-29 00:17:47 -0300

    Passando parametros pela rota

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,2 @@
+<h1>Teste de router</h1>
+<a href="/">Voltar para HOME</a>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'Pedro';
+    $idade = 24;
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'prof' => 'Dev']);
+});
+//Primeiro valor é a chave que vamos pegar no blade. EX: 'nome' = chave || $nome = valor
+
+Route::get('/teste', function() {
+    return view('teste');
 });
